fbfbcb32f666a0dec4cd3ca840505560a27f219d
Pubudhi <pywitharana@gmail.com>
2019-03-19 00:40:45 +0530

    Example for java does not consider about the order of the access modifiers and non-access modifiers

---
+++ Hello.java
@@ -0,0 +1,15 @@
+/*
+
+Example for java does not consider about the order of the access modifiers and non-access modifiers
+
+*/
+
+public class Hello{
+
+	static public void main(String[] args){
+	
+		System.out.println("Hello... This is your first application");
+
+	} 
+
+}
